378441a6b1f68e4321787dbe2d39f3b92cba9522
KyubumShim45854 <78337453+KyubumShim45854@users.noreply.github.com>
2021-03-22 11:34:18 +0900

    Update SpeedChecker.v

--- SpeedChecker.v
+++ SpeedChecker.v
@@ -1,32 +1,48 @@
 `timescale 1ns / 1ps
 
 module SpeedChecker(
-    input lightClk, secondClk, reset, start,
+    input lightClk, secondClk, reset, start,startCount,
     output [15:0] speedCheck
     );
     
     reg [8:0] pulseCount=0;
     reg [15:0] speedCheckPass=0;
     reg [4:0] currentTime=0;
+    reg [4:0] prevTime=0;
+    reg check=0;
     assign speedCheck=speedCheckPass;
     
-always @ (reset) begin
-   speedCheckPass<=0;
-    currentTime<=0;
-    pulseCount=0;
-end  
- always @ (posedge secondClk) begin
-    if(currentTime<10) begin          
-        if((pulseCount>=33)&&(speedCheckPass<9)) begin
-            speedCheckPass<=speedCheckPass+1;
+    
+    //reg posSec=0;
+    
+    always @(posedge secondClk) begin
+        if (reset) currentTime<=0;
+        else if(!start) currentTime<=currentTime;
+        else currentTime<=currentTime+1;
+    end
+
+
+always@(posedge lightClk) begin
+    if(reset) begin
+        speedCheckPass<=0;
+        pulseCount<=0;
+    end
+    else begin
+        if(start) pulseCount<=pulseCount+1;
+        else pulseCount<=pulseCount;
+        if(prevTime!=currentTime) begin
+            if(currentTime<=10) begin          
+                if((pulseCount>=33)&&(speedCheckPass<9)) begin
+                    speedCheckPass<=speedCheckPass+1;
+                end
+                else speedCheckPass<=speedCheckPass;
+            end
+            prevTime<=currentTime;
+            pulseCount<=0;
         end
-        currentTime<=currentTime+1;
+        
     end
     
-    pulseCount<=0;
-end
-always@(posedge lightClk) begin
-    if(start) pulseCount<=pulseCount+1;
 end    
     
     
